fbccf587d5e867c6ae3ebfe0b1ce158a14a6948b
PEDRO TROVO SOUZA <pedro.tsouza@solutis.net.br>
2025-05-20 14:57:44 -0300

    listando pedido

---
+++ src/main/java/com/acabou_o_mony/mony/mapper/MapperPedido.java
@@ -0,0 +1,14 @@
+package com.acabou_o_mony.mony.mapper;
+
+import com.acabou_o_mony.mony.dto.PedidoRequestDTO;
+import com.acabou_o_mony.mony.dto.PedidoResponseDTO;
+import com.acabou_o_mony.mony.entity.Pedido;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface MapperPedido {
+    Pedido toEntity(PedidoRequestDTO pedidoRequestDTO);
+    PedidoRequestDTO toPedidoRequestDTO(Pedido entity);
+    PedidoResponseDTO toPedidoResponseDTO(Pedido entity);
+}
+
--- src/main/java/com/acabou_o_mony/mony/service/PedidoService.java
+++ src/main/java/com/acabou_o_mony/mony/service/PedidoService.java
@@ -1,11 +1,27 @@
 package com.acabou_o_mony.mony.service;
 
+import com.acabou_o_mony.mony.dto.PedidoResponseDTO;
+import com.acabou_o_mony.mony.entity.Pedido;
+import com.acabou_o_mony.mony.mapper.MapperPedido;
 import com.acabou_o_mony.mony.repository.PedidoRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PedidoService {
 
+    @Autowired
     PedidoRepository pedidoRepository;
+
+    @Autowired
+    MapperPedido mapperPedido;
+
+    public PedidoResponseDTO buscarPedido(long id){
+        Pedido pedido = pedidoRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Pedido não encontrado com id: " + id));
+
+        return mapperPedido.toPedidoResponseDTO(pedido);
+    }
 
 }

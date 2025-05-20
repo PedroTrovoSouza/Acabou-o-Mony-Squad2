9e4530cd127520ae68e4efa2a496da0bf1d7a406
FABIO THIAGO DE AZEVEDO <fabio.azevedo@solutis.net.br>
2025-05-20 14:44:44 -0300

    teste

--- src/main/java/com/acabou_o_mony/mony/repository/CartaoRepository.java
+++ src/main/java/com/acabou_o_mony/mony/repository/CartaoRepository.java
@@ -2,6 +2,8 @@ package com.acabou_o_mony.mony.repository;
 
 import com.acabou_o_mony.mony.entity.Cartao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartaoRepository extends JpaRepository<Cartao, Long> {
 }
--- src/main/java/com/acabou_o_mony/mony/repository/ProdutoRepository.java
+++ src/main/java/com/acabou_o_mony/mony/repository/ProdutoRepository.java
@@ -1,4 +1,9 @@
 package com.acabou_o_mony.mony.repository;
 
-public interface ProdutoRepository {
+import com.acabou_o_mony.mony.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 }

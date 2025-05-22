ffc865b04805e86fffb128cfad81ee1fdd951457
Pedro Trovo <pedro.tsouza@solutis.com.br>
2025-05-22 10:04:42 -0300

    Update PedidoService.java

--- src/main/java/com/acabou_o_mony/mony/service/PedidoService.java
+++ src/main/java/com/acabou_o_mony/mony/service/PedidoService.java
@@ -1,49 +1,66 @@
-package com.acabou_o_mony.mony.controller;
+package com.acabou_o_mony.mony.service;
 
 import com.acabou_o_mony.mony.dto.PedidoCartaoProdutoDTO;
-import com.acabou_o_mony.mony.dto.PedidoRequestDTO;
+import com.acabou_o_mony.mony.dto.PedidoResponseDTO;
 import com.acabou_o_mony.mony.entity.Pedido;
+import com.acabou_o_mony.mony.mapper.MapperPedido;
+import com.acabou_o_mony.mony.repository.CartaoRepository;
+import com.acabou_o_mony.mony.repository.PedidoRepository;
 import com.acabou_o_mony.mony.repository.ProdutoRepository;
-import com.acabou_o_mony.mony.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
 
-@RestController
-@RequestMapping("/pedido")
-public class PedidoController {
+@Service
+public class PedidoService {
 
     @Autowired
-    PedidoService pedidoService;
+    PedidoRepository pedidoRepository;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getPedido(@PathVariable long id){
-        try {
-            return ResponseEntity.status(200).body(pedidoService.buscarPedido(id));
-        }catch (RuntimeException e){
-            return ResponseEntity.status(404).body(e.getMessage());
-        }
+    @Autowired
+    private CartaoRepository cartaoRepository;
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    @Autowired
+    MapperPedido mapperPedido;
+
+    public PedidoResponseDTO buscarPedido(long id){
+        PedidoResponseDTO pedido = pedidoRepository.buscarPedidoComNomeDoProduto(id);
+
+        return pedido;
     }
 
-    @PostMapping("/cadastrar")
-    public ResponseEntity<?> postPedido(@RequestBody PedidoCartaoProdutoDTO novoPedido) {
-        try {
-            PedidoCartaoProdutoDTO pedidoCriado = pedidoService.cadastrarPedido(novoPedido);
-            return ResponseEntity.status(HttpStatus.CREATED).body(pedidoCriado);
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    public PedidoCartaoProdutoDTO cadastrarPedido(PedidoCartaoProdutoDTO dto) {
+        System.out.println(" " + dto.getNomeProduto()+ " "+ dto.getNumeroCartao()+ " "+ " "+ " " +dto.getStatus()+ " "+dto.getValorTotal());
+        if (dto == null) {
+            throw new RuntimeException("Pedido não pode ser nulo");
         }
+
+        var cartao = cartaoRepository.findByNumero(dto.getNumeroCartao());
+        var produto = produtoRepository.findByNome(dto.getNomeProduto());
+
+        if (cartao.isEmpty() || produto.isEmpty()) {
+            throw new RuntimeException("Cartão ou Produto não encontrado");
+        }
+
+        Pedido pedido = mapperPedido.toEntityProduto(dto);
+        pedido.setCartao(cartao.get());
+        pedido.setProduto(produto.get());
+
+        Pedido salvo = pedidoRepository.save(pedido);
+
+        return mapperPedido.toPedidoCartaoProdutoDTO(salvo);
     }
 
-    @DeleteMapping("/cancelar/{numero}")
-    public ResponseEntity<?> deletePedido(@PathVariable long numero){
-        try {
-            Pedido pedidoDelete = pedidoService.deletePedido(numero);
-            return ResponseEntity.status(200).build();
-        }catch (RuntimeException e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
-    }
+    public Pedido deletePedido(long numero){
+        Pedido pedidoEncontrado = pedidoRepository.findByNumero(numero);
 
+        if (pedidoEncontrado == null){
+            throw new RuntimeException("Pedido não encontrado");
+        }
+
+        pedidoRepository.delete(pedidoEncontrado);
+        return pedidoEncontrado;
+    }
 }

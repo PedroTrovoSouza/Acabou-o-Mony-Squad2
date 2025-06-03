8a3faff388579e7c33237c8b2d7e136a434b0016
euFernandoAmoriim <fernandoalmeida.mda@gmail.com>
2025-06-03 15:15:14 -0300

    Adicionando GatewayController

--- Acabou_o_Mony_Microsservice/produto-service/src/main/java/com/acabou_o_mony/produto_service/controller/ProdutoController.java
+++ Acabou_o_Mony_Microsservice/produto-service/src/main/java/com/acabou_o_mony/produto_service/controller/ProdutoController.java
@@ -16,7 +16,7 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
-@RequestMapping("/produto")
+@RequestMapping("/produtos")
 @RequiredArgsConstructor
 public class ProdutoController {
 
